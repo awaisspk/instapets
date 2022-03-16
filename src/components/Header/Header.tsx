0f09a5a1f48767f934a2d599fecd8a9fec58b55c
awaisspk <iamawaisiqball@gmail.com>
2022-03-16 19:57:11 +0500

    fix: backdrop-blur issues

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -22,21 +22,23 @@ export const Header = () => {
   return (
     <header
       className={cx(
-        'flex fixed bg-blue-100/10 backdrop-blur-xl items-center w-full z-50 transition-all duration-500  dark:border-gray-800',
+        'flex fixed backdrop-blur-sm bg-white/60 items-center w-full z-50 transition-all duration-500  dark:border-gray-800',
         'dark:bg-black/5',
         {
-          'h-[70px] border-b-[1px] dark:border-gray-800 ': isScollPassed,
+          'h-[70px] border-b-[1px] dark:border-gray-800 dark:bg-black/30':
+            isScollPassed,
           'h-[100px]': !isScollPassed,
         }
       )}
     >
       <div className="flex justify-between mx-auto w-[min(1200px,100%-30px)]">
-        <div>
+        <div className="relative z-20">
           <Link href="/">
-            <a className="text-2xl md:text-3xl text-gray-700 dark:text-gray-200 font-bold select-none">
+            <a className="text-2xl relative md:text-3xl text-gray-700 dark:text-gray-200 font-bold select-none">
               Excelorythm
             </a>
           </Link>
+          <div className="absolute z-10 w-full h-full blur-xl" />
         </div>
         <ThemeToggle />
       </div>
