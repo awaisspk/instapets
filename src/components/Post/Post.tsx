5b8eb8cfcdf976a78bc43daa20fe08f2524fd786
awaisspk <iamawaisiqball@gmail.com>
2022-03-16 17:56:40 +0500

    feat: add user post component

---
+++ src/components/Post/Post.tsx
@@ -0,0 +1,85 @@
+import { useState } from 'react';
+
+import cx from 'classnames';
+import Image from 'next/image';
+import NextLink from 'next/link';
+
+import { Post } from '@/types';
+
+import { HeartIcon } from '../icons';
+import { Profile } from '../Profile';
+
+const Likes = ({ likes }: { likes: number }) => {
+  const [liked, setIsLiked] = useState(false);
+  return (
+    <div>
+      <button
+        onClick={() => setIsLiked((state) => !state)}
+        className="flex items-center"
+      >
+        <HeartIcon
+          className={cx('stroke-gray-600 w-8 md:w-9', {
+            'fill-red-600 stroke-red-600': liked,
+          })}
+        />
+        <span className="mt-1 ml-1">{liked ? likes + 1 : likes}</span>
+      </button>
+    </div>
+  );
+};
+
+type PostPreviewProps = Post & {
+  disableLink?: boolean;
+};
+
+export const PostPreview = (props: PostPreviewProps) => {
+  const { text, tags, likes, id, owner, publishDate, image, disableLink } =
+    props;
+  return (
+    <section className=" relative rounded-xl md:p-5 ">
+      <div className="backdrop-blur-xl z-10 mb-3">
+        <Profile
+          key={id}
+          date={publishDate}
+          firstName={owner.firstName}
+          lastName={owner.lastName}
+          picture={owner.picture}
+        />
+      </div>
+      <NextLink href={`/post/${id}`}>
+        <a
+          className={cx(
+            'relative block aspect-w-16 aspect-h-9 rounded-2xl overflow-hidden',
+            {
+              'pointer-events-none': disableLink,
+            }
+          )}
+        >
+          <Image
+            src={image}
+            alt=""
+            layout="fill"
+            className="hover:scale-105 transition-all"
+          />
+        </a>
+      </NextLink>
+      <div className="space-y-3">
+        <div className="flex items-center gap-3">
+          <Likes likes={likes} />
+          <div>
+            <p className="text-xs sm:text-sm mt-1">{text}</p>
+          </div>
+        </div>
+        <div className="flex gap-3">
+          {tags.map((tag, i) => (
+            <NextLink key={i} href={`/tags/${tag}`}>
+              <a className="bg-blue-200/50 hover:bg-blue-300/50 dark:bg-black/30 dark:hover:bg-black/40 text-sm px-3 py-1 rounded-full cursor-pointer">
+                {tag}
+              </a>
+            </NextLink>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -34,3 +34,20 @@ export const MoonIcon = (props: SVGProps<SVGSVGElement>) => (
     <path d="M12 3h.393a7.5 7.5 0 0 0 7.92 12.446A9 9 0 1 1 12 2.992z" />
   </svg>
 );
+
+export const HeartIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="44"
+    height="44"
+    viewBox="0 0 24 24"
+    strokeWidth="1.5"
+    fill="none"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <path d="M19.5 13.572l-7.5 7.428l-7.5 -7.428m0 0a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
+  </svg>
+);
