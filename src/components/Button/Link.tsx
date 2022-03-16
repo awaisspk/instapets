ff2b57af93826ca21d3b0f65030375c2aa5baf95
awaisspk <iamawaisiqball@gmail.com>
2022-03-16 17:55:03 +0500

    refactor: change link styles

--- src/components/Button/Link.tsx
+++ src/components/Button/Link.tsx
@@ -4,10 +4,7 @@ export const Link = (props: LinkProps) => {
   const { children, ...rest } = props;
   return (
     <>
-      <a
-        {...rest}
-        className="hover:underline underline-offset-1 text-gray-300 hover:text-gray-100"
-      >
+      <a {...rest} className="hover:underline underline-offset-1">
         {children}
       </a>
     </>
