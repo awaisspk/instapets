9d18e6e6e9577b76ef4b8836c52f8b558e540e3d
awaisspk <iamawaisiqball@gmail.com>
2022-03-16 19:00:05 +0500

    refactor: add top margin on footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import { Link } from '../Button';
 
 export const Footer = () => {
   return (
-    <footer className="flex justify-center py-3">
+    <footer className="flex justify-center mt-10 py-3">
       <span>
         Build by Awais iqbal.{' '}
         <Link target="_blank" href="https://github.com/awaisspk">
