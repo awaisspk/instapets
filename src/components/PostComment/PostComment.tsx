22b1357572d7dd0b91d3bbd9ac9333c994d042c5
awaisspk <iamawaisiqball@gmail.com>
2022-03-17 09:45:45 +0500

    fix: link user to profile page in comment section

--- src/components/PostComment/PostComment.tsx
+++ src/components/PostComment/PostComment.tsx
@@ -1,3 +1,5 @@
+import NextLink from 'next/link';
+
 import { Comment } from '@/types';
 
 import { Profile } from '../Profile';
@@ -7,12 +9,16 @@ export const PostComment = (comment: Comment) => {
   return (
     <section className="flex pb-10 flex-col border-b border-b-slate-300 dark:border-b-slate-300/30">
       <div className="space-y-3">
-        <Profile
-          firstName={owner.lastName}
-          lastName={owner.lastName}
-          picture={owner.picture}
-          date={rests.publishDate}
-        />
+        <NextLink href={`/profile/${owner.id}`}>
+          <a>
+            <Profile
+              firstName={owner.lastName}
+              lastName={owner.lastName}
+              picture={owner.picture}
+              date={rests.publishDate}
+            />
+          </a>
+        </NextLink>
         <p className="md:text-lg ml-20">{rests.message}</p>
       </div>
     </section>
